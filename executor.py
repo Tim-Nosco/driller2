df300e8f360c3d05fa64aa0a96462bebfab80a1b
tnosco <tnosco@home>
2018-11-27 15:39:51 -0500

    symbolic simgr runs forever with one active state

--- executor.py
+++ executor.py
@@ -2,17 +2,54 @@ import angr, claripy
 import logging
 
 root_logger = logging.getLogger()
-root_logger.setLevel(logging.DEBUG)
+root_logger.setLevel(logging.INFO)
 logger = logging.getLogger('executor.py')
 
+def hook(l=None):
+	#useful for testing
+	if l!=None:
+		locals().update(l)
+	import IPython
+	IPython.embed(banner1="", confirm_exit=False)
+	exit(0)
 
 def main():
 	base_path = "/job/target/"
-	bin_path = base_path+"CGC_Hangman_Game"
-	ld_path = base_path+"lib"
-	p = angr.Project(bin_path,
-		except_missing_libs=True,
-		custom_ld_path=ld_path)
+	ld_path  = base_path+"lib"
+	#inputs: CMD, input stream, input testcase filename
+	CMD = [base_path+"CGC_Hangman_Game"]
+	input_stream = "/dev/stdin"
+	input_testcase = base_path+"corpus/0"
+
+	#read testcase
+	with open(input_testcase, "rb") as f:
+		input_data = f.read()
+	#load the binary with the specified libraries
+	p = angr.Project(CMD[0], except_missing_libs=True, ld_path=(ld_path))
+
+	#initialize the concrete state with unicorn options and it's input data fixed
+	#TODO do not hardcode STDIN
+	concrete_state = p.factory.full_init_state(
+		add_options=angr.options.unicorn,
+		args = CMD,
+		stdin = input_data
+	)
+	concrete_sm = p.factory.simgr(concrete_state)
+
+	#initialize the symbolic state
+	symbolic_state = p.factory.full_init_state(
+		args = CMD
+	)
+	symbolic_sm = p.factory.simgr(symbolic_state)
+
+	#step both until symbolic_sm has more than one path
+	while len(symbolic_sm.active) == 1 and len(concrete_sm.active)==1:
+		logger.info("Stepping symbolic: %s", symbolic_sm)
+		symbolic_sm.step()
+		logger.info("Stepping concrete: %s", concrete_sm)
+		concrete_sm.step()
+
+	hook(locals())
 
 
 if __name__ == '__main__':
