12167e3395fbb98235a606efb89d663c31d46785
Timothy Nosco <tmnosco@ip-10-113-134-242.evoforge.org>
2018-12-13 18:11:19 +0000

    make log

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ all: build
 	docker run -it --rm -v /dev/shm:/dev/shm driller2 -v DEBUG -i /job/target/corpus/0 -l /job/target/lib /job/target/CGC_Hangman_Game
 	cp /dev/shm/corpus/* ./target/corpus
 
+log: build
+	docker run -i -w /job/target --rm --entrypoint /job/target/test.sh driller2 | tee log.txt
+
 build: clean
 	docker build -t driller2 .
 
--- target/test.sh
+++ target/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#find corpus/ -type f -exec bash -c 'echo {}; xxd {}; LD_LIBRARY_PATH=./lib ./CGC_Hangman_Game < {}; echo =====================' \; &>log.txt
 for i in corpus/*
 do
-        echo $i; xxd $i; LD_LIBRARY_PATH=./lib ./CGC_Hangman_Game < $i; echo -e \n=====================
+	echo $i; xxd $i; LD_LIBRARY_PATH=./lib ./CGC_Hangman_Game < $i; echo -e "\n====================="
 done
 
