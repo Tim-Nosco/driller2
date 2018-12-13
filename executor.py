987a1e39f683d92f15ed174e33c45e374c43d7b2
Timothy Nosco <tmnosco@ip-10-113-134-242.evoforge.org>
2018-12-13 16:28:34 +0000

    Add multiprocessing, remove extra sat timings.
    This helps reduce total time due to the extra sat calls being independant of the main branch's path.

--- executor.py
+++ executor.py
@@ -3,6 +3,8 @@ import angr, claripy
 import logging
 from hashlib import md5
 import time
+import multiprocessing as mp
+from functools import partial
 
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.WARNING)
@@ -16,54 +18,54 @@ def hook(l=None):
 	IPython.embed(banner1="", confirm_exit=False)
 	exit(0)
 
-def update_avg(start, cma, n):
-	xnp1 = time.time() - start
+def update_avg(xnp1, cma, n):
 	return cma+((xnp1 - cma)/(n+1)), n+1
 
-def main():
-	base_path = "/job/target/"
-	ld_path  = base_path+"lib"
-	#inputs: CMD, input stream, input testcase filename
-	CMD = [base_path+"CGC_Hangman_Game"]
-	corpus = base_path+"corpus/"
-	input_testcase = corpus+"0"
-	id_counter=0
-
+def main(command, corpus, testcase, ld_path):
 	#read testcase
-	with open(input_testcase, "rb") as f:
+	with open(testcase, "rb") as f:
 		input_data = f.read()
-	logger.info("Read %d bytes from testcase: %s.", len(input_data), input_testcase)
+	logger.info("Read %d bytes from testcase: %s.", len(input_data), testcase)
 
 	#load the binary with the specified libraries
 	logger.debug("Creating angr project.")
-	p = angr.Project(CMD[0], 
+	p = angr.Project(command[0], 
 		except_missing_libs=True, 
 		ld_path=(ld_path))
 	#create the entry state
 	logger.debug("Initializing entry state.")
 	s = p.factory.full_init_state(
 		mode="tracing",
-		args=CMD,
+		args=command,
 		stdin=angr.SimFileStream
 	)
 	#assert the current testcase
 	s.preconstrainer.preconstrain_file(input_data,s.posix.stdin,True)
 	#initialize the manager
-	simgr = p.factory.simgr(s, save_unsat=True, auto_drop={'missed', 'processed'})
+	simgr = p.factory.simgr(s, save_unsat=True)
 	#a state may be unsat only because of the file constraint
-	avg_extra_sat = (0.0, 0)
-	def valid_transition(state):
-		nonlocal avg_extra_sat
+	#use an id to produce reasonable file names
+	id_counter = 0
+	def valid_transition(state,counter):
 		#TODO: checkbitmap for necessity
 		logger.debug("Checking if %s is a valid transition.", state)
 		start = time.time()
 		state.preconstrainer.remove_preconstraints()
 		r = state.satisfiable()
-		avg_extra_sat = update_avg(start, *avg_extra_sat)
+		if r:
+			logger.info("Generated a new path!")
+			#pull out a valid stdin and write it to the corpus
+			data = state.posix.stdin.concretize()
+			name = "%s/id:%06d"%(corpus,counter,md5(data).hexdigest())
+			logger.debug("Saving %d bytes to %s", len(data), name)
+			with open(name, 'wb') as f:
+				f.write(data)
 		return r
+
 	#while there is a state in active
 	avg_step = (0.0, 0)
 	total_time = time.time()
+	processes = []
 	while simgr.active:
 		#make sure we're on a reasonable path
 		if len(simgr.active) > 1:
@@ -78,38 +80,35 @@ def main():
 		logger.debug("End:   %s", simgr)
 		#if states were unsat, check if they would have been valid
 		#without the stdin constraints
-		if simgr.unsat:
-			#save valid states to diverted
-			simgr.move(
-				from_stash='unsat', to_stash='diverted',
-				filter_func=valid_transition
-			)
-			#throw away the others
-			logger.debug("Clearing the unsat cache of %d states.", 
-				len(simgr.unsat))
-			simgr.move(from_stash='unsat', to_stash='missed')
-		for s in simgr.stashes['diverted']:
-			logger.info("Generated a new path!")
-			#pull out a valid stdin and write it to the corpus
-			data = s.posix.stdin.concretize()
-			name = "%s%03d_%s"%(corpus,id_counter,md5(data).hexdigest())
-			id_counter+=1
-			logger.debug("Saving %d bytes to %s", len(data), name)
-			with open(name, 'wb') as f:
-				f.write(data)
-		logger.debug("Clearing the diverted stash of %d states.", 
-			len(simgr.stashes['diverted']))
-		simgr.move(from_stash='diverted', to_stash='processed')
+		for s in simgr.unsat:
+			p = mp.Process(target=valid_transition, 
+				args=(s,id_counter))
+			p.start()
+			processes.append(p)
+			id_counter += 1
+		#throw away the others
+		logger.debug("Clearing the unsat cache of %d states.", 
+			len(simgr.unsat))
+		simgr.drop(stash='unsat')
+	for p in processes:
+		p.join()
 	total_time = time.time() - total_time
 	print("Time stepping concrete state: %.02fs %s" % (
 		avg_step[0]*avg_step[1], avg_step))
-	print("Time from extra sat calls:    %.02fs %s" % (
-		avg_extra_sat[0]*avg_extra_sat[1], avg_extra_sat))
 	print("Total runtime:                %.02fs" % total_time)
-	simgr.active
 	#hook(locals())
 
 
 if __name__ == '__main__':
 	logger.setLevel(logging.DEBUG)
-	main()
+	try:
+		logger.debug("Creating output directory")
+		os.mkdir("/dev/shm/corpus/")
+	except FileExistsError as e:
+		logger.warning("Corpus folder already exists")
+	main(
+		["/job/target/CGC_Hangman_Game"], 
+		"/dev/shm/corpus/", 
+		"/job/target/corpus/0",
+		"/job/target/lib"
+	)
